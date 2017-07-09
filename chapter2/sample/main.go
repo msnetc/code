ce21a7fd8b05c764ae6f6f77ca8217d72f3de81b
jerry <jerrymiao@renqidai.cc>
2017-07-09 16:51:28 +0800

    add

--- chapter2/sample/main.go
+++ chapter2/sample/main.go
@@ -3,9 +3,8 @@ package main
 import (
 	"log"
 	"os"
-
-	_ "github.com/goinaction/code/chapter2/sample/matchers"
-	"github.com/goinaction/code/chapter2/sample/search"
+	_"code/chapter2/sample/matchers"
+	"code/chapter2/sample/search"
 )
 
 // init is called prior to main.
